2a2473d7533b9b5c2183bc206496f0f332b8f718
AdolfoCampos <127461687+AdolfoCampos@users.noreply.github.com>
2025-06-28 17:13:54 -0600

    Leccion 3 If, Else, Elif

---
+++ main (5).py
@@ -0,0 +1,10 @@
+edad = int(input("¿Cuál es tu edad?"))
+if edad <= 13:
+    print("Eres un niño.")
+elif edad >= 14 and edad <= 17:
+    print("Eres un adolescente.")
+elif edad >= 18 and edad <= 64:
+    print("Eres un adulto.")
+else:
+    print("Eres un adulto mayor.")
+  
