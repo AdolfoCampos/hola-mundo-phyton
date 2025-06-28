89356ccf4472f91db16f34b590f3175605088a87
AdolfoCampos <127461687+AdolfoCampos@users.noreply.github.com>
2025-06-28 03:35:02 -0600

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,21 @@
+
+print("Hello Perros locos")
+print("Bueno me llamo Adolfo pero la banda me dice Fita y voy a ser programador")
+print("Tengo 30 años y soy de México")
+
+
+nombre = "Adolfo"
+edad = 30
+altura = 1.85
+es_estudiante = True
+
+print("Se llama:", nombre)
+print("Años de edad:", edad)
+print("Mide:", altura)
+print("¿Estudia?", es_estudiante)
+
+ciudad = "León"
+comida = "pizza"
+hermanos = 5
+
+print("Vivo en", ciudad, "me gusta mucho la", comida, "y tengo", hermanos, "hermanos")
